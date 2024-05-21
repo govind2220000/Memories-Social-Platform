e0f1a615b926f9b9367ec758c908d9cc0f5baa14
govind2220000 <72142346+govind2220000@users.noreply.github.com>
2024-05-21 14:24:41 +0530

    Initialized backend server with posts model controllers and routes

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/post.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// In summary, these lines are used to tell your server to accept any type of request body encoded in JSON format or URL-encoded format, with a maximum size of 30mb. The extended: true option allows for complex objects with nested properties to be parsed correctly. Without this, only simple key-value pairs would be parsed.
+
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL = process.env.MONGO_DB_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(console.log(err.message)));
