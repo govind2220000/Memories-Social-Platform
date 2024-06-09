7bcbaf5ec32ca7627c0c0e033c70860a4675e1b2
govind2220000 <72142346+govind2220000@users.noreply.github.com>
2024-06-09 16:36:50 +0530

    Added Update Post Route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose, { mongo } from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -18,7 +19,25 @@ export const createPost = async (req, res) => {
 
   try {
     await newPost.save(post);
-    res.status(201).json({ newPost });
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No Post with that id");
+  //When you set {new: true}, it means that the method will return the new version of the document after the update is applied
+
+  try {
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+      new: true,
+    });
+    res.status(201).json(updatedPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
