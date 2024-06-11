818a98aaa01f1336927dee3f5662891f2534a9a6
govind2220000 <72142346+govind2220000@users.noreply.github.com>
2024-06-11 12:21:22 +0530

    Added a default get route

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
 
 const CONNECTION_URL = process.env.MONGO_DB_URI;
 const PORT = process.env.PORT || 5000;
