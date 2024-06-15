6b0641a427eeebdcc49411092e716487599dbe0f
govind2220000 <72142346+govind2220000@users.noreply.github.com>
2024-06-15 23:57:25 +0530

    Added users routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/post.js";
+import userRoutes from "./routes/user.js";
 import dotenv from "dotenv";
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("Hello to Memories API");
 });
