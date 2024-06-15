8f5deeea75b3652200e38613c483750fcaf870ab
govind2220000 <72142346+govind2220000@users.noreply.github.com>
2024-06-15 23:44:35 +0530

    Added Sign in and Singup reusable  Input component

---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Grid, IconButton, InputAdornment, TextField } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+const Input = ({
+  name,
+  handleChange,
+  label,
+  half,
+  autoFocus,
+  type,
+  handleShowPassword,
+}) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        sx={{ padding: "2px 0px" }}
+        name={name}
+        onChange={handleChange}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password"
+            ? {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleShowPassword}>
+                      {type === "password" ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }
+            : null
+        }
+      ></TextField>
+    </Grid>
+  );
+};
+
+export default Input;
