0f87bb2c91592c3545fdc50b808454a253055050
govind2220000 <72142346+govind2220000@users.noreply.github.com>
2024-06-16 16:10:30 +0530

    Added env vari for google login auth key

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,23 @@ import { Container } from "@mui/material";
 
 import Navbar from "./components/Navbar/Navbar.js";
 import Home from "./components/Home/Home.js";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./components/Auth/Auth.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Container maxWidth="lg">
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <Container maxWidth="lg">
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   );
 };
 
