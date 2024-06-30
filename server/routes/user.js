19caa5d6f72070e08824f87e977d4efdafe8d05e
govind2220000 <72142346+govind2220000@users.noreply.github.com>
2024-07-01 01:08:52 +0530

    Added routes for reset and change password

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { signin, signup } from "../controllers/user.js";
+import {
+  signin,
+  signup,
+  forgotPassword,
+  resetPassword,
+  changePassword,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/signin", signin);
 router.post("/signup", signup);
+router.post("/forgotpassword", forgotPassword);
+router.get("/resetPassword/:id/:token", resetPassword);
+router.post("/resetPassword/:id/:token", changePassword);
 
 router.get("/", (req, res) => {
   res.send("Hello to User API");
